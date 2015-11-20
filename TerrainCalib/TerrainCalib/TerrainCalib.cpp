c6d7fcad8e9f09df36a1ea39f637198b2a5cf756
unknown <jm2232@cornell.edu>
2015-11-20 01:57:43 -0500

    new stuff

--- TerrainCalib/TerrainCalib/TerrainCalib.cpp
+++ TerrainCalib/TerrainCalib/TerrainCalib.cpp
@@ -2,22 +2,56 @@
 //
 
 #include "stdafx.h"
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp""
-#include "opencv2/imgproc/imgproc.hpp"
-#include "iostream"
+#include "TerrainCalib.h"
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
+
 using namespace cv;
 using namespace std;
+
 int main()
 {
 	Mat src;
 	src = imread("han.jpg", CV_LOAD_IMAGE_COLOR);
 	Mat gray;
 	cvtColor(src, gray, CV_BGR2GRAY);
-	imshow("gray1", gray);
-	GaussianBlur(gray,gray, Size(5,5), 0,0);
-	imshow("graygaus", gray);
-	threshold(gray, gray, 100, 255, THRESH_BINARY_INV); //Threshold the gray
+	/*imshow("gray1", gray);*/
+	GaussianBlur(src, src, Size(5, 5), 0, 0);
+	//imshow("graygaus", gray);*/
+	//threshold(gray, gray, 100, 255, THRESH_BINARY_INV); //Threshold the gray
+
+
+	//open text file containing points and store in array
+	ifstream infile;
+	infile.open("points.txt");
+
+	const size_t SIZE = 30;
+	size_t i = 0;
+	string line[SIZE];
+
+	while (!infile.eof) {
+		getline(infile, line[i]);
+		++i;
+	}
+
+	infile.close();
+
+	//iterate through all rows of input points
+	for (int i = 0; i < sizeof(line[0]); i++) {
+		Mat hsv;
+		cvtColor(src, hsv, CV_BGR2HSV);
+		Mat pointcvt = src(Rect(0, 0, 1, 1));
+		Mat image2;
+		cvtColor(pointcvt, image2, CV_BGR2HSV);
+
+		upper = line[]
+		lower =
+		//threshold(gray, gray, 100, 255, THRESH_BINARY_INV)
+
+
+	}
 	
 	imshow("gray", gray); int largest_area = 0;
 	int largest_contour_index = 0;
@@ -26,8 +60,10 @@ int main()
 	vector<Vec4i> hierarchy;
 	findContours(gray, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
 	Scalar color(255, 255, 0);  // color of the contour in the
+
+	write_to_obj_file("temp.obj", contours);
 	// iterate through each contour.
-	for (int i = 0; i< contours.size(); i++)
+	for (int i = 0; i < contours.size(); i++)
 	{
 		drawContours(src, contours, i, color, 5, 8, hierarchy);
 		////  Find the area of contour
@@ -40,14 +76,61 @@ int main()
 		//	bounding_rect = boundingRect(contours[i]);
 		//}
 	}
-	
+
 	//Draw the contour and rectangle
-	
+
 	//rectangle(src, bounding_rect, Scalar(0, 255, 0), 2, 8, 0);
 	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
 	imshow("Display window", src);
 	waitKey(0);
 	return 0;
+}
+
+/*
+Writes the list of contours to the given file.
+*/
+void write_to_obj_file(string file_name, const vector<cv::Mat> &contours){
+	//queue of vertex numbers representing start/end of a polygon. There is a polygon in between each two.
+	queue<int> polygons;
+	queue<Point> vertices;
+
+	int point_num = 0;
+	for (int i = 0; i < contours.size(); i++){
+		MatConstIterator_<Point> itr = contours[i].begin<Point>(), itr_end = contours[i].end<Point>();
+		for (; itr != itr_end; itr++){
+			Point p = *itr;
+			vertices.push(p);
+			point_num++;
+		}
+		polygons.push(point_num);
+	}
+
+	//Write polygon
+	if (polygons.size() > 0){
+		ofstream file(file_name);
+
+		//Write vertices
+		while (vertices.size() != 0){
+			Point p = vertices.front();
+			vertices.pop();
+			file << "v " << p.x << " " << p.y << "\n";
+		}
+
+		//Write polygons
+		int prev_v = 1;
+		while (polygons.size() != 0){
+			int lst_v = polygons.front();
+			polygons.pop();
+			
+			file << "f ";
+			for (int i = prev_v; i <= lst_v; i++){
+				file << i << " ";
+			}
+			file << "\n";
+
+			prev_v = lst_v + 1;
+		}
+	}
 }
 //#include "opencv2/highgui/highgui.hpp"
 //#include "opencv2/imgproc/imgproc.hpp"
---
+++ TerrainCalib/TerrainCalib/TerrainCalib.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace cv;
+using namespace std;
+
+int main();
+
+void write_to_obj_file(string file_name, const vector<cv::Mat> &contours);
